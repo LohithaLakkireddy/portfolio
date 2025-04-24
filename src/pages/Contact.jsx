24553436a15cc71a24998133535a7102a43ef0ea
LohithaLakkireddy <lakkireddylohitha9@gmail.com>
2025-04-24 14:21:41 +0530

    Update Contact.jsx

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -62,6 +62,7 @@ const Contact = () => {
               id="from_email"
               name="from_email"
               required
+              Pattern="^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,6}$"
               placeholder="you@example.com"
               title="Enter a valid email address"
               className="w-full px-4 py-2 rounded bg-primary text-white border border-gray-600 focus:outline-accent"
